a907c50b863711326ca76091bd61a84aeeea33fc
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-02 23:25:08 -0500

    Updated 'seeds' to populate DB with 'Item' records.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed DB with `Item` records.
+20.times do | i |
+    desc = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    is_preferred = [ true, false ].sample
+
+    item = Item.create( { :name => "Item #{i}", :description => desc, :preferred => is_preferred } )
+end
