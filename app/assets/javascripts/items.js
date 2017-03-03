e1e084d09e420fa012d72de6478d82502960f618
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-03-03 00:17:17 -0500

    Updated 'items.js' and 'ItemsController' to submit/save/print updates to 'Item' prop.

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$( document ).ready( function() {
+    $( 'form' ).on( 'click', function( e ) {
+        var form = $( e.target.parentElement );
+        var itemName = form.data( 'item-name' );
+
+        $.ajax( {
+            url: form.attr( 'action' ),
+            method: form.attr( 'method' ),
+            data: form.serialize()
+        } )
+        .done( function( response ) {
+            console.log( 'LOGGING OUT NEW VALUE FOR ' + itemName );
+            console.log( response );
+        } );
+    } );
+} );
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,21 @@ class ItemsController < ApplicationController
         @items = Item.all
     end
 
-    
+
     def update
-        # DO THE THING!
-    end
+        item = Item.find( params[ :id ] )
+
+        # Handle diff. request types:
+        respond_to do | format |
+            format.js do
+                # Set `preferred` equal to opposite boolean val.
+                item.preferred = !item.preferred
+                item.save
+
+                # Return new boolean value as plain text.
+                render ( { :plain => item.preferred.to_s } )
+            end
+        end
+    end # /update
 
 end
